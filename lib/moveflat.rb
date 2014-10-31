080757e8b7bc164f2969981aa32c9433f27eb67e
Alessandro <hello@alessndro.co.uk>
2014-10-31 00:02:33 +0000

    Links need 'www' prefixed

--- lib/moveflat.rb
+++ lib/moveflat.rb
@@ -4,9 +4,8 @@ require 'moveflat/scraper'
 require 'yaml'
 
 module Moveflat
-  BASE_URL = "http://moveflat.com"
+  BASE_URL = "http://www.moveflat.com"
 
-  # Image URLs need www
   BASE_IMAGE_URL = "http://www.moveflat.com/_ImageServer.aspx?"
 
   LONDON_AREAS = YAML::load_file("london_areas.yml")
